d002e149ac2f6441ef3d60c335d42a3e861c5da8
lilbugg0w0 <ljohn226@pratt.edu>
2026-02-27 13:53:15 -0500

    not sure

---
+++ SnailBug_Forest01/Assets/Scripts/MoveCam.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    public Transform cameraPos;
+     // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraPos.position;
+    }
+}
