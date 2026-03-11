926a931307d4fe0c8f128e117d1b3c20f51797b0
lilbugg0w0 <ljohn226@pratt.edu>
2026-03-11 13:19:54 -0400

    try2fix

--- SnailBug_Forest01/Assets/Scripts/FishBubbles.cs
+++ SnailBug_Forest01/Assets/Scripts/FishBubbles.cs
@@ -1,15 +1,7 @@
 using UnityEngine;
 
-public class RiverMovement : MonoBehaviour
+public class FishBubbles : MonoBehaviour
 {
-
-    //bubble vars
-    public GameObject bubbleFab;
-    public int bubbleCount = 3;
-    public float spawnRadius = 4f;
-
-    //kitttyyyyyyyyy :3
-    public KittyAI kitty;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,8 +11,6 @@ public class RiverMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-
+        
     }
-
 }
--- SnailBug_Forest01/Assets/Scripts/KittyAI.cs
+++ SnailBug_Forest01/Assets/Scripts/KittyAI.cs
@@ -1,18 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class KittyAI : MonoBehaviour
 {
     public enum KittyStates
     {
-        trapped,
+        trapped,        //want cat to sit
         followOnGround,
         fish,
-        idle
+        idle        //want cat to sit
     }
 
     [Header("Move Controls")]
     public float followSpeed;
-    public float avoidRadius;
+    public float avoidRadius; 
+    public float slowToIdleDist;
     public float idleDistance;
 
     [Header("Behavior controls")]
@@ -24,13 +26,14 @@ public class KittyAI : MonoBehaviour
 
     private bool isFishing;   //true when fishing
     private bool isInRiver;   //true when senses river collider
-    private bool inFishRange; //becomes true when sense river bank collider
+    private bool inFishRange = false; //becomes true when sense river bank collider
 
     [Header("Fishing Properties")]
-    public float fishCooldown;
+    public float fishCooldown = 5f;
+    public RiverBehavior RiverBehavior;
     //public float fishTime;
-    public KeyCode fishKey;
 
+    public KeyCode fishKey = KeyCode.F;
     //References
     private Rigidbody rb;
     public Transform cameraTransform;
@@ -50,10 +53,25 @@ public class KittyAI : MonoBehaviour
         //if player hasString and collides with cat, current = follow
         if (collision.collider.CompareTag("Player"))
         {
-            playerTransform = GameObject.FindGameObjectWithTag("Player").transform; 
+                playerTransform = GameObject.FindGameObjectWithTag("Player").transform; 
+        }
+
+        if (collision.collider.CompareTag("RiverBank"))
+        {
+            inFishRange = true;
+            Debug.Log("Collided with riverbank");
+            RiverBehavior.StartCoroutineExternal(playerTransform);
+                
         }
     }
 
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("RiverBank"))
+        {
+            inFishRange = false;
+        }
+    }
     private void CatAction()
     {
         //a switch case searches for your current state,
@@ -62,22 +80,27 @@ public class KittyAI : MonoBehaviour
         {
             case KittyStates.trapped: 
               //if player is equal to or out of idle dist, = follow
-                if ((playerTransform.position - transform.position).magnitude >= idleDistance) currentState = KittyStates.followOnGround;
-                if ((playerTransform.position - transform.position).magnitude < idleDistance) currentState = KittyStates.idle;
+              if(playerTransform != null)
+                {
+                    if ((playerTransform.position - transform.position).magnitude >= idleDistance)currentState = KittyStates.followOnGround;
+                    if ((playerTransform.position - transform.position).magnitude < idleDistance)currentState = KittyStates.idle;
+                }
+
                 TrappedInTree();
-                break;
+                break; 
 
             case KittyStates.followOnGround:
               //if player not moving and player is in idle range, current = idle 
                 if ((playerTransform.position - transform.position).magnitude < idleDistance) currentState = KittyStates.idle;
-                if (!isInRiver && inFishRange && fishCooldown <= 0 && Input.GetKeyDown(fishKey)) currentState = KittyStates.fish;
+                if (inFishRange && fishCooldown <= 0 && Input.GetKeyDown(fishKey)) currentState = KittyStates.fish;
                 FollowingPlayer();
                 break; 
 
             case KittyStates.fish:
                 //if fishing time is done, current = idle
                 if ((playerTransform.position - transform.position).magnitude >= idleDistance) currentState = KittyStates.followOnGround;
-                if ((playerTransform.position - transform.position).magnitude < idleDistance) currentState = KittyStates.idle;
+                //if ((playerTransform.position - transform.position).magnitude < idleDistance) currentState = KittyStates.idle;
+                if (inFishRange && fishCooldown > 0 && inFishRange) currentState = KittyStates.idle;
                 Fish();
                 break;
 
@@ -85,24 +108,67 @@ public class KittyAI : MonoBehaviour
               //if player is in fish range AND not touching river
               //AND fishCooldown = 0 AND press fishKey, current = fishing
               //if player is moving AND is out of idle range, current = follow
-                if (!isInRiver && inFishRange && fishCooldown  <= 0 && Input.GetKeyDown(fishKey)) currentState = KittyStates.fish;
+                if (inFishRange && fishCooldown  <= 0 && Input.GetKeyDown(fishKey)) currentState = KittyStates.fish;
                 if ((playerTransform.position - transform.position).magnitude >= idleDistance) currentState = KittyStates.followOnGround;
                 break;
         }
     }
 
+    /* FISHING LOGIC:
+River spawns bubbles
+Player clicks river to start fishing
+Bubble appears
+Player clicks bubble
+Kitty runs to bubble and swipes
+If kitty reaches bubble in time, catch fish
+If notfish escapes
+    */
+
     //when does each behavior end? 
     void TrappedInTree() 
     {
         //cat sit and look around
-        currentState = KittyStates.trapped;
+       
     }
+    public LayerMask avoidLayer;
 
+    private Vector3 GetAvoidVector()
+    {
+        //calculating avoid mechanic
+        Collider[] objectsInRadius = Physics.OverlapSphere(transform.position, avoidRadius, avoidLayer);
+        Vector3 avoidVector = Vector3.zero;
+        for (int i = 0; i < objectsInRadius.Length; i++)
+        {
+            Vector3 difference = transform.position - objectsInRadius[i].ClosestPoint(transform.position);
+            avoidVector += difference.normalized / difference.magnitude;
+
+        }
+        return avoidVector;
+    }
     void FollowingPlayer()
     {
+        //avoid obstacles      
+        Vector3 moveVelocity = GetAvoidVector() * avoidObjectsBias;
+        moveVelocity += (playerTransform.position - transform.position).normalized * targetBias;
+            
+
         //follow player at follow distance
-        //avoid obstacles
-        currentState = KittyStates.followOnGround;
+        Vector3 playerDirection = playerTransform.position - transform.position;
+        moveVelocity = moveVelocity.normalized;
+        float speedMultiplier = followSpeed;
+
+        //make cat slow into idle
+        float slowDownAmount = Mathf.Clamp(Vector3.Dot(moveVelocity, playerDirection.normalized), 0f, 1f);
+        slowDownAmount *= 1 - Mathf.Clamp(playerDirection.magnitude, 0f, slowToIdleDist) / slowToIdleDist;
+        speedMultiplier -= followSpeed * slowDownAmount;
+
+        rb.linearVelocity = moveVelocity.normalized * speedMultiplier;
+        transform.rotation = Quaternion.LookRotation(-moveVelocity.normalized);
+    }
+
+    public void StartFishing(Vector3 hitPoint)
+    {
+        
     }
 
     void Fish()
@@ -110,8 +176,36 @@ public class KittyAI : MonoBehaviour
        //cat swipe at water bubbles
        //based on timing, fishing successful or unsuccessful
        //start fish cooldown
-       isFishing = true;
-       currentState = KittyStates.fish;
+
+        if (!isFishing) //if is not fishing, start fishing 
+        {
+            fishCooldown = 5;
+            isFishing = true;        
+            
+        }
+
+        
+
+        if (isFishing)
+        {
+            
+        }
+       //currentState = KittyStates.fish;
     }
 
+    public void CatchFish()
+    {
+        Debug.Log("Kitty caught a fish!");
+       // currentState = KittyStates.fish;
+
+        // play catch animation here
+    
+    }
+
+    public void MissFish()
+    {
+        Debug.Log("Fish escaped!");
+
+        // play fail animation
+    }
 }
